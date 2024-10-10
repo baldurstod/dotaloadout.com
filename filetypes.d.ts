4ccbbec0edf73708d60b0f8312d7ae43021c0a90
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-10-10 18:15:13 +0200

    Added tsconfig.json, filetypes.d.ts

---
+++ filetypes.d.ts
@@ -0,0 +1,24 @@
+declare module '*.fs' {
+	const value: string
+	export default value
+}
+declare module '*.vs' {
+	const value: string
+	export default value
+}
+declare module '*.glsl' {
+	const value: string
+	export default value
+}
+declare module '*.css' {
+	const value: string
+	export default value
+}
+declare module '*.wasm' {
+	const value: string
+	export default value
+}
+declare module '*.json' {
+	const value: string
+	export default value
+}
