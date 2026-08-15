bc7fc4899981ba9df306b8f15703760abe31a008
Guillaume <108530065+baldurstod@users.noreply.github.com>
2026-08-15 21:53:15 +0200

    Added some icons on the hero selector

--- src/client/ts/view/utils/createcharacterelement.ts
+++ src/client/ts/view/utils/createcharacterelement.ts
@@ -8,7 +8,7 @@ export function createCharacterElement(characterTemplate: CharacterTemplate): HT
 		class: 'character-selector-character',
 		childs: [
 			createElement('div', {
-				class: characterTemplate.isHero() ? 'hero-icon' : `world-slot-${characterTemplate.id}`,
+				class: 'icon ' + (characterTemplate.isHero() ? 'hero-icon' : `world-slot-${characterTemplate.id}`),
 				// Hero order id starts at 1
 				// we use heroCount - 1 to acknowledge the fact that 0% means top is aligned with top edge and 100% bottom is aligned with bottom edge
 				...(characterTemplate.isHero()) && { style: `background-position-y:${(characterTemplate.heroOrderId - 1) / (heroCount - 1) * 100}%` },
